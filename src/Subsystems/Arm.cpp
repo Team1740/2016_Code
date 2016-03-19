fddf8adcc4fe69d17c2f8008f6a2aec85e93eb9c
FRC Team 1740 Deus Ex Coder <cestabrooks@ledyard.net>
2016-03-19 12:18:05 -0400

    Just a Test
    This is Trevor, just commented out the arm camera commands in Arm.cpp
    because it is already written in Robot.cpp.

--- src/Subsystems/Arm.cpp
+++ src/Subsystems/Arm.cpp
@@ -8,6 +8,7 @@ Arm::Arm(): Subsystem("Arm")
 	lifterEncoder = new Encoder(LIFTER_ENCODER_PORT_4, LIFTER_ENCODER_PORT_5);
 	extenderEncoder = new Encoder(EXTENDER_ENCODER_PORT_6, EXTENDER_ENCODER_PORT_7);
 	armCamera = new USBCamera("cam0", true);
-	armCamera->OpenCamera();
-	armCamera->StartCapture();
+	//armCamera->OpenCamera();
+	//armCamera->StartCapture();
+	//armCamera->GetInstance()->StartAutomaticCapture("cam0");
 }
